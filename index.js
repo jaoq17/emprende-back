ebe2744d1e4dc4d1b900e16d8638a8f732d54f72
Asistenteplaneacion <apoyo.planeacion@fadp.edu.co>
2024-03-05 16:24:29 -0500

    Control de peticiones

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-require('dotenv').config()
+require('dotenv').config();
 const  http = require("http");
+const fs = require("fs");
+
 
 // const exportsFromAnother = require("./another")
 // console.log({ http})
 
-function requestController(){
-    
+function requestController(req, res){
+    const url = req.url
+    const method = req.method
+    console.log({ url, method })
+
+    if (method === "GET" && url === "/"){
+        res.setHeader("Content-type", "text/html; charset=utf-8")
+        fs.readFile("./public/index.html", function(err, file){
+            if (err){
+                console.log("Hubo un error")
+            }
+        
+            res.write(file)
+            res.end()
+            })
+            return
+    }
+
+    if (method === "GET" && url === "/about"){
+        res.setHeader("Content-type", "text/html; charset=utf-8")
+        fs.readFile("./public/about.html", function(err, file){
+            if (err){
+                console.log("Hubo un error")
+            }
+        
+            res.write(file)
+            res.end()
+            })
+            return
+    }
+
+    res.setHeader("Content-type", "text/html; charset=utf-8")
+    res.write("<h1>Pagina no encontrada</h1>")
+    res.end()
 }
+
 
 //configurar nuestro servidor
 const server = http.createServer(requestController)
