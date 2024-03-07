f925ef442ee4729b15652547d7bf9ee99e0bca7c
Asistenteplaneacion <apoyo.planeacion@fadp.edu.co>
2024-03-07 14:34:25 -0500

    cambios

--- server-node.js
+++ server-node.js
@@ -1,61 +1,61 @@
-require('dotenv').config();
-const  http = require("http");
-const fs = require("fs");
+// require('dotenv').config();
+// const  http = require("http");
+// const fs = require("fs");
 
 
-// const exportsFromAnother = require("./another")
-// console.log({ http})
+// // const exportsFromAnother = require("./another")
+// // console.log({ http})
 
-function requestController(req, res){
-    const url = req.url
-    const method = req.method
-    console.log({ url, method })
+// function requestController(req, res){
+//     const url = req.url
+//     const method = req.method
+//     console.log({ url, method })
 
-    if (method === "GET" && url === "/"){
-        res.setHeader("Content-type", "text/html; charset=utf-8")
-        fs.readFile("./public/index.html", function(err, file){
-            if (err){
-                console.log("Hubo un error")
-            }
+//     if (method === "GET" && url === "/"){
+//         res.setHeader("Content-type", "text/html; charset=utf-8")
+//         fs.readFile("./public/index.html", function(err, file){
+//             if (err){
+//                 console.log("Hubo un error")
+//             }
         
-            res.write(file)
-            res.end()
-            })
-            return
-    }
+//             res.write(file)
+//             res.end()
+//             })
+//             return
+//     }
 
-    if (method === "GET" && url === "/about"){
-        res.setHeader("Content-type", "text/html; charset=utf-8")
-        fs.readFile("./public/about.html", function(err, file){
-        /* Valores que evaluados en un contexto BOOLEANO, arrojan FALSY:
-            a) null
-            b) undefined
-            c) 0
-            d) ""
-            e) false
-            f) NaN  ej: si declaro una variable como number y le paso un string
-        */
-            if (err){
-                console.log("Hubo un error")
-            }
+//     if (method === "GET" && url === "/about"){
+//         res.setHeader("Content-type", "text/html; charset=utf-8")
+//         fs.readFile("./public/about.html", function(err, file){
+//         /* Valores que evaluados en un contexto BOOLEANO, arrojan FALSY:
+//             a) null
+//             b) undefined
+//             c) 0
+//             d) ""
+//             e) false
+//             f) NaN  ej: si declaro una variable como number y le paso un string
+//         */
+//             if (err){
+//                 console.log("Hubo un error")
+//             }
         
-            res.write(file)
-            res.end()
-            })
-            return
-    }
+//             res.write(file)
+//             res.end()
+//             })
+//             return
+//     }
 
-    res.setHeader("Content-type", "text/html; charset=utf-8")
-    res.write("<h1>Pagina no encontrada</h1>")
-    res.end()
-}
+//     res.setHeader("Content-type", "text/html; charset=utf-8")
+//     res.write("<h1>Pagina no encontrada</h1>")
+//     res.end()
+// }
 
 
-//configurar nuestro servidor
-const server = http.createServer(requestController)
+// //configurar nuestro servidor
+// const server = http.createServer(requestController)
 
-const PORT = process.env.PORT
+// const PORT = process.env.PORT
 
-server.listen(process.env.PORT, function(){
-    console.log("Aplicacion corriendo en puerto: " + PORT)
-})
+// server.listen(process.env.PORT, function(){
+//     console.log("Aplicacion corriendo en puerto: " + PORT)
+// })
