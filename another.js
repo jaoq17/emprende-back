2d3f1192c8906d90bce284f686920bbb2614f4ca
Asistenteplaneacion <apoyo.planeacion@fadp.edu.co>
2024-03-12 14:03:53 -0500

    cambios

---
+++ another.js
@@ -0,0 +1,11 @@
+// const name = "martin"
+
+
+// console.log({ module})
+// module.exports = ({ name })
+// console.log(module.exports)
+
+// module.exports = "exporto lo que quiera"
+
+// const person = { name: "Martin"}
+// console.log(person.name) // Dot notation : para leer una propiedad u obtener el valor ej person.name
